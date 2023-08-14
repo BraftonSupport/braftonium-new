5ff9a7b5bc9f647986ff0027bbbc28fe05c9fcab
Deryk w. King <dking3876@msn.com>
2023-08-14 18:16:37 -0400

    update to omit use for gravity forms as it produces and error via rest api to render gravity forms

--- gutenberg-addon/build/index.asset.php
+++ gutenberg-addon/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => 'dde19caddedae0d0a0c67679a374b1c6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '162e450286455abdceeba2cf0c0d5c00');
