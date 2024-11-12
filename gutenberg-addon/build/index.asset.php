e363478eaa345fa68f12efc90620213569bfbfea
Deryk w. King <dking3876@msn.com>
2024-11-12 13:26:16 -0500

    update packages for wordpress version 6.6+

--- gutenberg-addon/build/index.asset.php
+++ gutenberg-addon/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '162e450286455abdceeba2cf0c0d5c00');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '577b6752a0f9d596e932fe2cee8ad129');
